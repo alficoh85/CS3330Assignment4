02344f4b888ad32358f35f918dcfa796cf9bb8ee
Alex Cohen <alficoh85@gmail.com>
2025-04-22 14:45:36 -0500

    Finally getting Brett's changes to my branch

--- src/hw4/maze/CellComponents.java
+++ src/hw4/maze/CellComponents.java
@@ -1,10 +1,23 @@
 package hw4.maze;
 
+import java.util.Random;
+
 /**
  * This is an enum with three cell components: wall, aperture, and exit.
  */
 public enum CellComponents {
 	WALL,
 	APERTURE,
-	EXIT
+	EXIT;
+
+	public static CellComponents randomWallOrAperture() {
+		Random random = new Random();
+		boolean randBoolean = random.nextBoolean();
+		if (randBoolean == true) {
+			return WALL;
+		}
+		else {
+			return APERTURE;
+		}
+	}
 }
