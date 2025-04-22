e576e3f0c7ae9eed8db29e8bd7d0e1fc65b1926c
WoodySmith2004 <bsmith_04@yahoo.com>
2025-04-22 02:00:36 -0500

    agent logic with expectation of correction

--- src/hw4/player/Player.java
+++ src/hw4/player/Player.java
@@ -1,5 +1,35 @@
 package hw4.player;
 
-public class Player {
+import hw4.maze.Cell;
+import hw4.maze.CellComponents;
+import hw4.maze.Row;
 
-}
+public class Player {
+    private Row currentRow;
+    private Cell currentCell;
+
+    public Player(Row row, Cell cell) {
+        this.currentRow = row;
+        this.currentCell = cell;
+    }
+
+    public Row getCurrentRow() {
+        return currentRow;
+    }
+
+    public Cell getCurrentCell() {
+        return currentCell;
+    }
+
+    public void moveTo(Row newRow, Cell newCell) {
+        if (newRow != null && newCell != null) {
+            this.currentRow = newRow;
+            this.currentCell = newCell;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Player [currentCell=" + currentCell + ", currentRow=" + currentRow + "]";
+    }
+} 
